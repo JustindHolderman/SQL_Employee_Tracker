f13788534190bca9ce9697246368520415d1370e
JustindHolderman <160930788+JustindHolderman@users.noreply.github.com>
2024-08-08 15:53:19 -0500

    Added inquirer prompts, and set up tableWorker.

---
+++ db/schema.sql
@@ -0,0 +1,35 @@
+-- delete existing company_info database and create a new one
+DROP DATABASE IF EXISTS company_info;
+CREATE DATABASE company_info;
+
+-- connect to the company_info database
+CREATE TABLE department (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
+);
+
+-- create the role table
+CREATE TABLE role (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE CASCADE
+);
+
+-- create the employee table
+CREATE TABLE employee (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE CASCADE,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
+);
